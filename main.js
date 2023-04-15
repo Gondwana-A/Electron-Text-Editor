d1b96ca400b3a97990968d1e096495007899aebe
alexpopov86 <alexanderpopov1986@outlook.com>
2023-04-15 11:43:23 +0900

    start point

---
+++ main.js
@@ -0,0 +1,10 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+function createWindow() {
+	let win = new BrowserWindow({width: 800, height: 600})
+	win.loadFile('index.html')
+}
+
+app.on('ready', createWindow)
